6d5eab9c63174bb1592a2495aa63f5fbcc7bdb8b
sergivalverde <sergivalv@gmail.com>
2018-09-21 22:32:06 +0200

    adding some data utilies

---
+++ data_utils.py
@@ -0,0 +1,147 @@
+# --------------------------------------------------
+#
+# data utils
+# to doc
+#
+# Sergi Valverde
+#
+# --------------------------------------------------
+
+import numpy as np
+from operator import add
+
+# from sklearn.feature_extraction.image import extract_patches_sk
+
+
+def get_voxel_coordenates(input_data, roi, step_size=1):
+    """
+    Get voxel coordenates based on some pre-defined rule or input mask.
+    For each selected voxel, return its (x,y,z) coordinate.
+
+    inputs:
+    - input_data (useful for extracting non-zero voxels)
+    - roi: region of interest to extract samples
+    - step_size: sampling overlap
+    """
+
+    x, y, z = np.where(roi > 0)
+    voxel_coords = [(x_, y_, z_)
+                    for x_, y_, z_, s in zip(x, y, z, range(len(x)))
+                    if s % step_size == 0]
+
+    return voxel_coords
+
+
+def get_patches(input_data, centers, patch_size=(15, 15, 15)):
+    """
+    Get image patches of arbitrary size based on a set of voxel coordenates
+
+    inputs:
+    - input_data: a tridimensional np.array matrix
+    - centers:  centre voxel coordenate for each patch
+    - patch_size: patch size (x,y,z)
+
+    outputs:
+    - patches: np.array containing each of the patches
+    """
+    # If the size has even numbers, the patch will be centered. If not,
+    # it will try to create an square almost centered. By doing this we allow
+    # pooling when using encoders/unets.
+    patches = []
+    list_of_tuples = all([isinstance(center, tuple) for center in centers])
+    sizes_match = [len(center) == len(patch_size) for center in centers]
+
+    if list_of_tuples and sizes_match:
+        # apply padding to the input image and re-compute the voxel coordenates
+        # according to the new dimension
+        padded_image = apply_padding(input_data, patch_size)
+        patch_half = tuple([idx // 2 for idx in patch_size])
+        new_centers = [map(add, center, patch_half) for center in centers]
+        # compute patch locations
+        slices = [[slice(c_idx-p_idx, c_idx+s_idx-p_idx)
+                   for (c_idx, p_idx, s_idx) in zip(center,
+                                                    patch_half,
+                                                    patch_size)]
+                  for center in new_centers]
+
+        # extact patches
+        patches = [padded_image[idx] for idx in slices]
+
+    return np.array(patches)
+
+
+def recontruct_image(input_data, centers, output_size):
+    """
+    Reconstruct image based on several ovelapping patch samples
+
+    inputs:
+    - input_data: a np.array list with patches
+    - centers: center voxel coordenates for each patch
+    - output_size: output image size (x,y,z)
+    """
+
+    # apply a padding around edges before writing the results
+    # recompute the voxel dimensions
+    patch_size = input_data[0, :].shape
+    out_image = apply_padding(np.zeros(output_size), patch_size)
+    patch_half = tuple([idx // 2 for idx in patch_size])
+    new_centers = [map(add, center, patch_half) for center in centers]
+    # compute patch locations
+    slices = [[slice(c_idx-p_idx, c_idx+s_idx-p_idx)
+               for (c_idx, p_idx, s_idx) in zip(center,
+                                                patch_half,
+                                                patch_size)]
+              for center in new_centers]
+
+    # for each patch, sum it to the output patch and
+    # then update the frequency matrix
+
+    freq_count = np.zeros_like(out_image)
+
+    for patch, slide in zip(input_data, slices):
+        out_image[slide] += patch
+        freq_count[slide] += np.ones(patch_size)
+
+    # invert the padding applied for patch writing
+    out_image = invert_padding(out_image, patch_size)
+    freq_count = invert_padding(freq_count, patch_size)
+
+    # return the mean of all the patches
+    return out_image / freq_count
+
+
+def apply_padding(input_data, patch_size, mode='constant', value=0):
+    """
+    Apply padding to edges in order to avoid overflow
+
+    """
+
+    patch_half = tuple([idx // 2 for idx in patch_size])
+    padding = tuple((idx, size-idx)
+                    for idx, size in zip(patch_half, patch_size))
+
+    padded_image = np.pad(input_data,
+                          padding,
+                          mode=mode,
+                          constant_values=value)
+
+    return padded_image
+
+
+def invert_padding(padded_image, patch_size):
+    """
+    Invert paadding on edges to recover the original shape
+
+    inputs:
+    - padded_image defined by apply_padding function
+    - patch_size (x,y,z)
+
+    """
+
+    patch_half = tuple([idx // 2 for idx in patch_size])
+    padding = tuple((idx, size-idx)
+                    for idx, size in zip(patch_half, patch_size))
+
+    return padded_image[padding[0][0]:-padding[0][1],
+                        padding[1][0]:-padding[1][1],
+                        padding[2][0]:-padding[2][1]]
